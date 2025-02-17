9d5556793042f1298ba6c3e57c5518e57e2237a0
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-02-17 15:01:43 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Framework./MyGame.h"
+
+
+
+//Windowsアプリのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	std::unique_ptr<Framework> game = std::make_unique<MyGame>();
+
+	game->Run();
+
+	return 0;// main関数のリターン
+}
+
