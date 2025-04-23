eca975f6fb0bf8fb7af59829e7b02435e5d708b6
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-04-23 23:29:20 +0900

    制御点の修正

--- Application/SpriteApp/ScreenApp/TitleScreen.h
+++ Application/SpriteApp/ScreenApp/TitleScreen.h
@@ -31,10 +31,10 @@ private:
 	std::unique_ptr<UIBase> background_[3];
 	uint32_t numBGs_ = 3;
 
-	//std::unique_ptr<UIBase> option_[6];
-	//uint32_t numOptions_ = 6;
-	//uint32_t keyBoardOption_ = 3;
-	//uint32_t controllerOption_ = 4;
+	std::unique_ptr<UIBase> option_[6];
+	uint32_t numOptions_ = 6;
+	uint32_t keyBoardOption_ = 3;
+	uint32_t controllerOption_ = 4;
 
 
 };
