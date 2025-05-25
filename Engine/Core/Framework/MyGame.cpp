119c20da676607caf29cd82bc2d12d72baaedda6
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-05-26 01:23:16 +0900

    タイトルにパーティクル生成

--- Engine/Core/Framework/MyGame.cpp
+++ Engine/Core/Framework/MyGame.cpp
@@ -17,7 +17,7 @@ void MyGame::Initialize()
 	offScreen_->Initialize();
 	//offScreen_->SetEffectType(OffScreen::OffScreenEffectType::RadialBlur);
 #ifdef _DEBUG
-	SceneManager::GetInstance()->ChangeScene("Title");
+	SceneManager::GetInstance()->ChangeScene("Game");
 #else//Title
 	SceneManager::GetInstance()->ChangeScene("Title");
 #endif
@@ -33,7 +33,7 @@ void MyGame::Initialize()
 	
 	ParticleManager::GetInstance()->CreateParticleGroup("GrowthParticle", defaultTexturePath + "growth.png");
 	ParticleManager::GetInstance()->CreateParticleGroup("YodareParticle", defaultTexturePath + "yodare.png");
-	ParticleManager::GetInstance()->CreateParticleGroup("TitleParticle", defaultTexturePath + "circle.png");
+	ParticleManager::GetInstance()->CreateParticleGroup("TitleParticle", defaultTexturePath + "circle2.png");
 
 
 	ParticleManager::GetInstance()->SetPrimitiveMesh("GrowthParticle", planeMesh);
