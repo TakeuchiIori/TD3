1b22daed688010d6c2319ec4d2edfee1ab252767
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-05-27 12:39:02 +0900

    斜め45度に吹っ飛ぶように

--- Application/Objects/EnemyManager/BaseEnemy.cpp
+++ Application/Objects/EnemyManager/BaseEnemy.cpp
@@ -31,7 +31,7 @@ void BaseEnemy::KnockBack()
 	// === ノックバック中の処理 ===
 	if (knockbackTimer_ > 0.0f && isTakeAttack_) {
 		worldTransform_.translation_ += knockbackVelocity_;
-		knockbackVelocity_ *= 0.85f; // 減衰
+		knockbackVelocity_ *= 0.9f; // 減衰
 		knockbackTimer_ -= 1.0f / 60.0f;
 		worldTransform_.UpdateMatrix();
 		obbCollider_->Update();
