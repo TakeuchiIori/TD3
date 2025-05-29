6b50808329634b461bc725762806d28cd8a7fe79
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-05-29 14:12:26 +0900

    プレイヤーの頭の位置を変更

--- Application/Stage/Stage.cpp
+++ Application/Stage/Stage.cpp
@@ -32,7 +32,6 @@ void Stage::InitCheckPoint()
 	player_->Reset();
 	pos.x = StageEditor::Instance()->GetInitX(currentStageNum_, currentCheckPoint_);
 	player_->SetTimeLimit(StageEditor::Instance()->GetTimeLimit(currentStageNum_, currentCheckPoint_));
-	player_->SetPos({ pos.x, 4.0f, pos.z });
 	ReloadObject();
 }
 
