ecd488e421af5d4e684d48b5fb94fcd4edd7a43d
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-05-31 22:40:10 +0900

    Titleから始まるように

--- Engine/Core/Framework/MyGame.cpp
+++ Engine/Core/Framework/MyGame.cpp
@@ -17,7 +17,7 @@ void MyGame::Initialize()
 	offScreen_->Initialize();
 	//offScreen_->SetEffectType(OffScreen::OffScreenEffectType::RadialBlur);
 #ifdef _DEBUG
-	SceneManager::GetInstance()->ChangeScene("Clear");
+	SceneManager::GetInstance()->ChangeScene("Title");
 #else//Title
 	SceneManager::GetInstance()->ChangeScene("Title");
 #endif
