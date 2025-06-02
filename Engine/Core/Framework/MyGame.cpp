d5c63221359ffacabdc7dbb84ef919e3aef5f044
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2025-06-02 20:56:43 +0900

    komitto

--- Engine/Core/Framework/MyGame.cpp
+++ Engine/Core/Framework/MyGame.cpp
@@ -17,7 +17,7 @@ void MyGame::Initialize()
 	offScreen_->Initialize();
 	//offScreen_->SetEffectType(OffScreen::OffScreenEffectType::RadialBlur);
 #ifdef _DEBUG
-	SceneManager::GetInstance()->ChangeScene("Clear");
+	SceneManager::GetInstance()->ChangeScene("Game");
 #else//Title
 	SceneManager::GetInstance()->ChangeScene("Title");
 #endif
